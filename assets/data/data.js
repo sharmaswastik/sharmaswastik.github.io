ddca62c90780f51095dd74b818d1fe4c8f3428ed
Swastik Sharma <48920344+sharmaswastik@users.noreply.github.com>
2026-02-09 15:44:57 +0530

    Update data.js

--- assets/data/data.js
+++ assets/data/data.js
@@ -150,12 +150,12 @@ const siteData = {
             "title": "IDSO-Managed Bid-Based Transactive Design for DER Participation in Wholesale Markets While Preserving T-D Interactions",
             "authors": "S. Sharma, S. Battula, and S.N. Singh",
             "year": "2026",
-            "venue": "IEEE Transactions on Energy Markets, Policy, and Regulation",
+            "venue": "IEEE Transactions on Energy Markets, Policy and Regulation",
             "link": "https://ieeexplore.ieee.org/document/11373199",
             "preprint": "https://arxiv.org/abs/2508.08187",
             "code": "https://doi.org/10.5281/zenodo.17582538",
             "doi": "doi: 10.1109/TEMPR.2026.3661757",
-            "citation": "S. Sharma, S. Battula and S. N. Singh, “IDSO-Managed Bid-Based Transactive Distribution Systems Design for DER Participation in Wholesale Markets While Preserving T-D Interactions,” IEEE Transactions on Energy Markets, Policy and Regulation, doi: 10.1109/TEMPR.2026.3661757."
+            "citation": "S. Sharma, S. Battula and S. N. Singh, “IDSO-Managed Bid-Based Transactive Design for DER Participation in Wholesale Markets While Preserving T-D Interactions,” IEEE Transactions on Energy Markets, Policy and Regulation, doi: 10.1109/TEMPR.2026.3661757."
         }],
         "conferences": [
             {
